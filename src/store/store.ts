dad86b070a0ba7aa76c2a0f85cdfcca0d4901a83
Pavel Novik <brn2befree@gmail.com>
2023-11-06 21:05:32 +0300

    finish counter 1 and refactoring with Redux

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,15 +2,19 @@ type StateType = {
     counter: number
     maxVal: number
     startVal: number
+    isSettings: boolean
     isError: boolean
     userMessage: null | string
 }
 
 export type StoreType = {
     _state: StateType
-    getState: ()=> StateType
-    increaseCount: ()=> void
-    changeMaxVal: ()=> void
+    getState: () => StateType
+    increaseCount: () => void
+    resetCount: () => void
+    changeMaxVal: (value: string) => void
+    changeStartVal: (value: string) => void
+    setChanges: ()=> void
 }
 
 export const store: StoreType = {
@@ -18,6 +22,7 @@ export const store: StoreType = {
         counter: 0,
         maxVal: 5,
         startVal: 0,
+        isSettings: false,
         isError: false,
         userMessage: null
     },
@@ -25,8 +30,26 @@ export const store: StoreType = {
         return this._state
     },
     increaseCount() {
-        this._state.counter +=1
+        this._state.counter += 1
     },
-    changeMaxVal() {},
+    resetCount() {
+        this._state.counter = this._state.startVal
+    },
+    changeMaxVal(value: string) {
+        const maxVal = +value
+        this._state.isSettings = true
+        maxVal <= this._state.startVal ? this._state.isError = true : this._state.isError = false
+        this._state.maxVal = maxVal
+    },
+    changeStartVal(value: string) {
+        const startVal = +value
+        this._state.isSettings = true
+        startVal < 0 ? this._state.isError = true : this._state.isError = false
+        this._state.maxVal = startVal
+    },
+    setChanges() {
+        this._state.counter = this._state.startVal
+        this._state.isSettings = false
+    }
 
 }
