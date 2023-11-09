6dda598ed3c1fed92af32fb2ce47d1461531710f
Pavel Novik <brn2befree@gmail.com>
2023-11-09 16:26:57 +0300

    refactoring counter 1 with Redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import Counter from "./components/Counter/Counter";
 import Settings from "./components/Settings/Settings";
-import {StoreType} from "./store/store";
+import {
+    changeMaxValAC,
+    changeStartValAC,
+    increaseCountAC,
+    resetCountAC,
+    setCounterAC,
+    setErrorAC,
+    StoreType
+} from "./store/store";
 
 type AppPropsType = {
     store: StoreType
 }
 
-function App(props: AppPropsType) {
-    const store = props.store
-    const [counter, setCounter] = useState(0)
-    const [maxVal, setMaxVal] = useState(5)
-    const [startVal, setStartVal] = useState(0)
-    const [isError, setIsError] = useState(false)
+function App({store}: AppPropsType) {
+    const state = store.getState()
+    // const [counter, setCounter] = useState(0)
+    const counter = state.counter
+    // const [maxVal, setMaxVal] = useState(5)
+    const maxVal = state.maxVal
+    // const [startVal, setStartVal] = useState(0)
+    const startVal = state.startVal
+    // const [isError, setIsError] = useState(false)
+    const isError = state.isError
     const [userMessage, setUserMessage] = useState<null | string>(null)
 
     useEffect(() => {
@@ -21,52 +33,62 @@ function App(props: AppPropsType) {
         const currStartVal = localStorage.getItem('startVal')
         const currMaxVal = localStorage.getItem('maxVal')
         if (currVal) {
-            setCounter(JSON.parse(currVal))
+            // setCounter(JSON.parse(currVal))
+            store.dispatch(setCounterAC(+currVal))
         }
         if (currStartVal) {
-            setStartVal(JSON.parse(currStartVal))
+            // setStartVal(JSON.parse(currStartVal))
+            store.dispatch(changeStartValAC(+currStartVal))
         }
         if (currMaxVal) {
-            setMaxVal(JSON.parse(currMaxVal))
+            // setMaxVal(JSON.parse(currMaxVal))
+            store.dispatch(changeMaxValAC(+currMaxVal))
         }
     }, []);
 
-    useEffect(() => {
-        if (startVal < 0 || startVal >= maxVal) {
-            setIsError(true)
-            setUserMessage('incorrect value!')
-        } else {
-            setIsError(false)
-            // setUserMessage('enter values and press \'set\'')
-        }
-    }, [startVal, maxVal]);
+    // useEffect(() => {
+    //     if (startVal < 0 || startVal >= maxVal) {
+    //         // setIsError(true)
+    //         store.dispatch(setErrorAC(true))
+    //         setUserMessage('incorrect value!')
+    //     } else {
+    //         // setIsError(false)
+    //         store.dispatch(setErrorAC(false))
+    //         setUserMessage('enter values and press \'set\'')
+    //     }
+    // }, [startVal, maxVal]);
     // useEffect(() => {
     //
     // }, [counter]);
 
     const resetCounterHandler = () => {
-        setCounter(startVal)
-        localStorage.setItem('countVal', JSON.stringify(startVal))
+        // setCounter(startVal)
+        store.dispatch(resetCountAC())
+        // localStorage.setItem('countVal', JSON.stringify(startVal))
     }
     const increaseCounterHandler = () => {
-        setCounter(prevState => {
-            const newState = prevState + 1
-            localStorage.setItem('countVal', JSON.stringify(newState))
-            return newState
-        })
+        // setCounter(prevState => {
+        //     const newState = prevState + 1
+        //     localStorage.setItem('countVal', JSON.stringify(newState))
+        //     return newState
+        // })
+        store.dispatch(increaseCountAC())
     }
     const onChangeMaxVal = (value: string) => {
-        setMaxVal(JSON.parse(value))
+        // setMaxVal(JSON.parse(value))
+        store.dispatch(changeMaxValAC(+value))
         setUserMessage('enter values and press \'set\'')
     }
     const onChangeStartVal = (value: string) => {
-        setStartVal(JSON.parse(value))
+        // setStartVal(JSON.parse(value))
+        store.dispatch(changeStartValAC(+value))
         setUserMessage('enter values and press \'set\'')
     }
     const setSettingsHandler = () => {
-        localStorage.setItem('maxVal', JSON.stringify(maxVal))
-        localStorage.setItem('startVal', JSON.stringify(startVal))
-        setCounter(startVal)
+        // localStorage.setItem('maxVal', JSON.stringify(maxVal))
+        // localStorage.setItem('startVal', JSON.stringify(startVal))
+        // setCounter(startVal)
+        store.dispatch(setCounterAC(startVal))
         setUserMessage(null)
     }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -39,29 +39,48 @@ export const store: StoreType = {
         switch (action.type) {
             case 'INCREASE_COUNT': {
                 this._state.counter += 1
+                localStorage.setItem('countVal', JSON.stringify(this._state.counter))
+                this._subscriber()
                 return this._state
             }
             case 'RESET_COUNT': {
                 this._state.counter = this._state.startVal
+                this._subscriber()
+                return this._state
+            }
+            case 'SET_COUNTER': {
+                this._state.counter = action.payload.value
+                // this._state.isSettings = false
+                this._subscriber()
                 return this._state
             }
             case 'CHANGE_MAX_VAL': {
-                const maxVal = +action.payload.value
+                const maxVal = action.payload.value
                 this._state.isSettings = true
-                maxVal <= this._state.startVal ? this._state.isError = true : this._state.isError = false
+                maxVal <= this._state.startVal || maxVal < 0 || this._state.startVal < 0 ? this._state.isError = true : this._state.isError = false
                 this._state.maxVal = maxVal
+                localStorage.setItem('maxVal', JSON.stringify(this._state.maxVal))
+                this._subscriber()
                 return this._state
             }
             case 'CHANGE_START_VAL': {
-                const startVal = +action.payload.value
+                const startVal = action.payload.value
                 this._state.isSettings = true
-                startVal < 0 ? this._state.isError = true : this._state.isError = false
-                this._state.maxVal = startVal
+                startVal < 0 || startVal >= this._state.maxVal || this._state.maxVal < 0? this._state.isError = true : this._state.isError = false
+                this._state.startVal = startVal
+                localStorage.setItem('startVal', JSON.stringify(this._state.startVal))
+                this._subscriber()
                 return this._state
             }
             case 'SET_CHANGES': {
                 this._state.counter = this._state.startVal
                 this._state.isSettings = false
+                this._subscriber()
+                return this._state
+            }
+            case 'SET_ERROR': {
+                this._state.isError = action.payload.isError
+                this._subscriber()
                 return this._state
             }
             default: {
@@ -103,6 +122,8 @@ type ActionDispatchType =
     | ChangeMaxValACType
     | ChangeStartValACType
     | SetChangesType
+    | SetCounterType
+    | SetErrorACType
 
 type IncreaseCountACType = ReturnType<typeof increaseCountAC>
 export const increaseCountAC = () => {
@@ -111,6 +132,25 @@ export const increaseCountAC = () => {
         payload: {}
     } as const
 }
+type SetCounterType = ReturnType<typeof setCounterAC>
+export const setCounterAC = (value: number) => {
+    return {
+        type: 'SET_COUNTER',
+        payload: {
+            value
+        }
+    } as const
+}
+type SetErrorACType = ReturnType<typeof setErrorAC>
+export const setErrorAC = (isError: boolean) => {
+    return {
+        type: 'SET_ERROR',
+        payload: {
+            isError
+        }
+    } as const
+}
+
 type ResetCountACType = ReturnType<typeof resetCountAC>
 export const resetCountAC = () => {
     return {
@@ -119,7 +159,7 @@ export const resetCountAC = () => {
     } as const
 }
 type ChangeMaxValACType = ReturnType<typeof changeMaxValAC>
-export const changeMaxValAC = (value: string) => {
+export const changeMaxValAC = (value: number) => {
     return {
         type: 'CHANGE_MAX_VAL',
         payload: {
@@ -128,7 +168,7 @@ export const changeMaxValAC = (value: string) => {
     } as const
 }
 type ChangeStartValACType = ReturnType<typeof changeStartValAC>
-export const changeStartValAC = (value: string) => {
+export const changeStartValAC = (value: number) => {
     return {
         type: 'CHANGE_START_VAL',
         payload: {
