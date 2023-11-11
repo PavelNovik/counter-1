40164a1d1ee3df2d7f4da0aa152ad233c107f5b2
Pavel Novik <brn2befree@gmail.com>
2023-11-11 09:48:31 +0300

    refactoring counter 1 with Redux

--- src/components/Input/Input_Styles.ts
+++ src/components/Input/Input_Styles.ts
@@ -1,20 +1,29 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 type InputType = {
     error: boolean
 }
 
  const Input_Styles = styled.input<InputType>`
-  background-color: ${props => props.error ? '#fa6a6a' : 'white'};
-  border-color: ${props => props.error ? 'red' : 'default'};
-  outline-color: ${props => props.error ? 'red' : 'default'};
-  text-align: center;
-  margin-left: 50px;
-  height: 40px;
-  border-radius: 10px;
-  font-size: 15px;
-  font-weight: bold;
-`
+     // background-color: ${props => props.error ? '#fa6a6a' : 'white'};
+     // border-color: ${props => props.error ? 'red' : 'default'};
+     // outline-color: ${props => props.error ? 'red' : 'default'};
+   background-color: white;
+   border-color: transparent;
+   outline-color: cadetblue;
+   text-align: center;
+   margin-left: 50px;
+   height: 40px;
+   border-radius: 10px;
+   font-size: 15px;
+   font-weight: bold;
+
+   ${props => props.error && css<InputType>`
+     background-color: #fa6a6a;
+     border-color: red;
+     outline-color: red;
+   `}
+ `
 
 export const S = {
     Input: Input_Styles
